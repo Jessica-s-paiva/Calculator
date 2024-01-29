0078b6a700c7b2f1e9da98f223e9af09de926212
jessica.paiva <jessicapaivamvp@gmail.com>
2024-01-28 23:52:25 -0300

    Desevolvimento dos métodos da calculadora

---
+++ src/main/java/com/curso/java/calculator/model/Calculate.java
@@ -0,0 +1,5 @@
+package com.curso.java.calculator.model;
+
+public class Calculate {
+
+}
--- src/main/java/com/curso/java/calculator/util/CalculateUtils.java
+++ src/main/java/com/curso/java/calculator/util/CalculateUtils.java
@@ -1,19 +1,78 @@
 package com.curso.java.calculator.util;
 
+import com.curso.java.calculator.model.User;
+
+import java.util.Scanner;
+
 public class CalculateUtils {
-    public static double Sum(Double numberOne, Double numberTwo){
-        return numberOne + numberTwo;
+    private static final Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
+
+    public static void calculate(){
+        String operation;
+        Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
+        double a, b;
+        User user = new User("Jess", "Analista de sistemas", 33, "(41)987990725");
+
+        System.out.println("Bem vindo(a) " + user.getName() + "! \nVi que você é um(a) " + user.getProfession() + "!");
+        System.out.println("Digite qual operação você deseja realizar: Soma, Subtração, Multiplicação ou Divisão?");
+        operation = ler.next();
+
+        switch (operation){
+            case "soma":
+                sum();
+                break;
+
+            case "subtração":
+                subtraction();
+                break;
+
+            case "multiplicação":
+                multiplication();
+                break;
+
+            case "divisão":
+                division();
+                break;
+        }
     }
 
-    public static double Subtract(Double numberOne, Double numberTwo){
-        return numberOne - numberTwo;
+    public static void sum(){
+        System.out.println("Informe o primeiro valor: ");
+        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("Informe o segundo valor.: ");
+        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("O resultado da sua soma é: " + ( numberOne + numberTwo));
     }
 
-    public static double Multiplication(Double numberOne, Double numberTwo){
-        return numberOne * numberTwo;
+    public static void subtraction(){
+        System.out.println("Informe o primeiro valor: ");
+        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("Informe o segundo valor.: ");
+        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("O resultado da sua subtração é: " + ( numberOne - numberTwo));
     }
 
-    public static double Division(Double numberOne, Double numberTwo){
-        return numberOne / numberTwo;
+    public static void multiplication(){
+        System.out.println("Informe o primeiro valor: ");
+        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("Informe o segundo valor.: ");
+        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("O resultado da sua multiplicação é: " + ( numberOne * numberTwo));
+    }
+
+    public static void division(){
+        System.out.println("Informe o primeiro valor: ");
+        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        System.out.println("Informe o segundo valor.: ");
+        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+
+        if (numberTwo == 0){
+            System.out.println("Erro, o número divisor deve ser diferente de zero!");
+            System.out.println("Informe o primeiro valor: ");
+            numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+            System.out.println("Informe o segundo valor.: ");
+            numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        }
+        System.out.println("O resultado da sua divisão é: " + ( numberOne / numberTwo));
     }
 }
