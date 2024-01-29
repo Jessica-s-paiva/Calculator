a601af2fb647b63502b0a6d7214cc8e1589a95a3
jessica.paiva <jessicapaivamvp@gmail.com>
2024-01-29 00:45:03 -0300

    Ajustes nos métodos e alguns casos de tratamento de erro

--- src/main/java/com/curso/java/calculator/util/CalculateUtils.java
+++ src/main/java/com/curso/java/calculator/util/CalculateUtils.java
@@ -8,70 +8,80 @@ public class CalculateUtils {
     private static final Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
 
     public static void calculate(){
-        String operation;
-        Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
-        double a, b;
-        User user = new User("Jess", "Analista de sistemas", 33, "(41)987990725");
+        while (true){
+            String operation;
+            Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
+            User user = new User("Jess", "Analista de sistemas", 33, "(41)987990725");
 
-        System.out.println("Bem vindo(a) " + user.getName() + "! \nVi que você é um(a) " + user.getProfession() + "!");
-        System.out.println("Digite qual operação você deseja realizar: Soma, Subtração, Multiplicação ou Divisão?");
-        operation = ler.next();
+            System.out.println("Bem vindo(a) " + user.getName() + "! \nVi que você é um(a) " + user.getProfession() + "!");
+            System.out.println("Digite qual operação você deseja realizar: Soma, Subtração, Multiplicação ou Divisão?");
+            operation = removeAccent(ler.next()).toLowerCase().replace("ç", "c" +
+                    "c");
 
-        switch (operation){
-            case "soma":
-                sum();
-                break;
+            switch (operation){
+                case "soma":
+                    sum();
+                    break;
 
-            case "subtração":
-                subtraction();
-                break;
+                case "subtracao":
+                    subtraction();
+                    break;
 
-            case "multiplicação":
-                multiplication();
-                break;
+                case "multiplicacao":
+                    multiplication();
+                    break;
 
-            case "divisão":
-                division();
-                break;
+                case "divisao":
+                    division();
+                    break;
+
+                default:
+                    System.out.println("Opção inválida, digite uma operação válida!");
+            }
         }
+    }
+
+    public static String removeAccent(String input) {
+        return java.text.Normalizer.normalize(input, java.text.Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
     }
 
     public static void sum(){
         System.out.println("Informe o primeiro valor: ");
-        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberOne = ler.nextDouble();
         System.out.println("Informe o segundo valor.: ");
-        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberTwo = ler.nextDouble();
         System.out.println("O resultado da sua soma é: " + ( numberOne + numberTwo));
     }
 
     public static void subtraction(){
         System.out.println("Informe o primeiro valor: ");
-        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberOne = ler.nextDouble();
         System.out.println("Informe o segundo valor.: ");
-        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberTwo = ler.nextDouble();
         System.out.println("O resultado da sua subtração é: " + ( numberOne - numberTwo));
     }
 
     public static void multiplication(){
         System.out.println("Informe o primeiro valor: ");
-        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberOne = ler.nextDouble();
         System.out.println("Informe o segundo valor.: ");
-        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberTwo = ler.nextDouble();
         System.out.println("O resultado da sua multiplicação é: " + ( numberOne * numberTwo));
     }
 
     public static void division(){
         System.out.println("Informe o primeiro valor: ");
-        double numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberOne = ler.nextDouble();
         System.out.println("Informe o segundo valor.: ");
-        double numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+        double numberTwo = ler.nextDouble();
 
         if (numberTwo == 0){
             System.out.println("Erro, o número divisor deve ser diferente de zero!");
             System.out.println("Informe o primeiro valor: ");
-            numberOne = ler.nextDouble(); // 3. entrada de dados (lendo um valor inteiro)
+            numberOne = ler.nextDouble();
             System.out.println("Informe o segundo valor.: ");
-            numberTwo = (double) ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+            numberTwo = ler.nextDouble();
         }
         System.out.println("O resultado da sua divisão é: " + ( numberOne / numberTwo));
     }
